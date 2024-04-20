a179e48a83987f4631af694cb1e1f65f773ea5f1
Matei Stanca <i@ambientimpact.com>
2024-04-20 12:00:39 -0400

    Tooltip: Added a max-width to override Tippy.js default.

--- javascript/tooltips.js
+++ javascript/tooltips.js
@@ -14,6 +14,10 @@ AmbientImpact.addComponent('baseThemeTooltips', function(baseTooltips, $) {
   // means - such as in a theme that uses this base theme.
   tippy.setDefaultProps({
     animation: 'shift-away',
+    // This seems to be only way to get Tippy.js to not output any inline
+    // max-width at all so that we can instead set this via a stylesheet. Any
+    // value apparently works here.
+    maxWidth: '',
     // Our custom Material theme; not to be confused with the one that ships
     // with Tippy.js which is not loaded.
     theme: 'material',
