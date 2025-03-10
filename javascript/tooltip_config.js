9d0afa0f46814e111c84ced99fc2475907fee6dc
Matei Stanca <i@ambientimpact.com>
2025-03-09 21:21:55 -0400

    Improved white-space formatting in tooltip_config.js props.

--- javascript/tooltip_config.js
+++ javascript/tooltip_config.js
@@ -12,14 +12,18 @@ AmbientImpact.onGlobals([
   //
   // @see https://atomiks.github.io/tippyjs/v6/all-props/
   tippy.setDefaultProps({
+
     animation: 'shift-away',
+
     // This seems to be only way to get Tippy.js to not output any inline
     // max-width at all so that we can instead set this via a stylesheet. Any
     // value apparently works here.
     maxWidth: '',
+
     // Our custom Material theme; not to be confused with the one that ships
     // with Tippy.js which is not loaded.
     theme: 'material',
+
   });
 
 });
