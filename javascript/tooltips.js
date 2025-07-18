667e391f7cedef1155a2126d1a1f85270d9078b6
Matei Stanca <i@ambientimpact.com>
2025-07-18 13:38:28 -0400

    Fixed title attribute tooltips not attaching on RefreshLess restore:
    This affected any elements that had had a title attribute tooltip shown
    that had been subsequently cached and then restored by RefreshLess.

--- javascript/tooltips.js
+++ javascript/tooltips.js
@@ -2,16 +2,41 @@
 //   Ambient.Impact - Base - Tooltips
 // -----------------------------------------------------------------------------
 
-AmbientImpact.on(['tooltip'], function(aiTooltip) {
-AmbientImpact.addComponent('baseThemeTooltips', function(baseTooltips, $) {
+AmbientImpact.on(['fastdom', 'tooltip'], function(aiFastDom, aiTooltip) {
+AmbientImpact.addComponent('baseThemeTooltips', (baseTooltips, $) => {
 
   'use strict';
+
+  /**
+   * FastDom instance.
+   *
+   * @type {FastDom}
+   */
+  const fastdom = aiFastDom.getInstance();
 
   this.addBehaviour(
     'AmbientImpactTooltips',
     'ambientimpact-tooltips',
     '.layout-container',
     function(context, settings) {
+
+      // Restore any title attributes that were left as data attributes as can
+      // occur when restoring from RefreshLess' cache.
+      //
+      // @todo Remove when we can reliably detach before caching.
+      $(this).find('[data-original-title]').each(async (i, element) => {
+
+        const $this = $(element);
+
+        await fastdom.mutate(() => {
+
+          $this.attr('title', $this.attr('data-original-title')).removeAttr(
+            'data-original-title',
+          );
+
+        });
+
+      });
 
       $(this).prop(
         'AmbientImpactTooltips',
@@ -21,7 +46,7 @@ AmbientImpact.addComponent('baseThemeTooltips', function(baseTooltips, $) {
     },
     function(context, settings, trigger) {
 
-      $(this).prop('AmbientImpactTooltips').destroy();
+      $(this).prop('AmbientImpactTooltips')?.destroy();
 
       $(this).removeProp('AmbientImpactTooltips');
 
