c5296b4fe3fe75647f775916bd435e879ccbc507
Matei Stanca <i@ambientimpact.com>
2025-07-21 21:02:29 -0400

    Reworked tooltip RefreshLess cache handling to act on cached snapshot.

--- javascript/refreshless.cache.js
+++ javascript/refreshless.cache.js
@@ -35,7 +35,6 @@ AmbientImpact.addComponent('baseThemeRefreshLessCache', (component, $) => {
     '.content-popup-offcanvas-button',
     '.offcanvas-overlay',
     '.pswp',
-    '[data-tippy-root]',
   ];
 
   $(once(
--- javascript/tooltips.js
+++ javascript/tooltips.js
@@ -18,25 +18,10 @@ AmbientImpact.addComponent('baseThemeTooltips', (baseTooltips, $) => {
     'AmbientImpactTooltips',
     'ambientimpact-tooltips',
     '.layout-container',
+    // We want to ignore 'refreshless:before-cache' but we do want to be
+    // notified when displaying a cached snapshot.
+    ['unload', 'refreshless:cached-snapshot'],
     function(context, settings) {
-
-      // Restore any title attributes that were left as data attributes as can
-      // occur when restoring from RefreshLess' cache.
-      //
-      // @todo Remove when we can reliably detach before caching.
-      $(this).find('[data-original-title]').each(async (i, element) => {
-
-        const $this = $(element);
-
-        await fastdom.mutate(() => {
-
-          $this.attr('title', $this.attr('data-original-title')).removeAttr(
-            'data-original-title',
-          );
-
-        });
-
-      });
 
       $(this).prop(
         'AmbientImpactTooltips',
@@ -45,6 +30,34 @@ AmbientImpact.addComponent('baseThemeTooltips', (baseTooltips, $) => {
 
     },
     function(context, settings, trigger) {
+
+      // Remove any cached element and return if displaying a cached snapshot.
+      if (trigger === 'refreshless:cached-snapshot') {
+
+        // Restore any title attributes that were left as data attributes as can
+        // occur when restoring from RefreshLess' cache.
+        $(this).find('[data-original-title]').each(async (i, element) => {
+
+          const $this = $(element);
+
+          await fastdom.mutate(() => {
+
+            $this.attr('title', $this.attr('data-original-title')).removeAttr(
+              'data-original-title',
+            );
+
+            // Find the Tooltip itself, if it exists, and remove it as well.
+            $(`#${$this.attr('aria-describedby')}`).remove();
+
+            $this.removeAttr('aria-describedby');
+
+          });
+
+        });
+
+        return;
+
+      }
 
       $(this).prop('AmbientImpactTooltips')?.destroy();
 
